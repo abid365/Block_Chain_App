b43720d7e6781b8c51f66b4968b3dc5d5adc8c07
jawad Bin Azam <abidibnazam@gmail.com>
2023-08-02 01:04:14 +0600

    save

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-
+import { HiMenuAlt4 } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
 const NavBar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <nav className="w-full flex md:justify-center justify-between items-center p-4">
+      <div className="md:flex-[0.5] flex-initial justify-center items-center"></div>
+    </nav>
   );
 };
 
