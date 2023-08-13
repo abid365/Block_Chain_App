4266d12c38b7c234ddc100535cb702445307b27b
jawad Bin Azam <abidibnazam@gmail.com>
2023-08-13 22:53:15 +0600

    folder commit

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -13,6 +13,19 @@ const Welcome = () => {
     console.log("Yay!");
   };
 
+  // Input component
+  // to write jsx code use -> () instead of {}
+  const Input = ({ placeholder, name, type, value, handleChange }) => (
+    <input
+      placeholder={placeholder}
+      type={type}
+      step="0.0001"
+      value={value}
+      onChange={(e) => handleChange(e, name)}
+      className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+    />
+  );
+
   return (
     <div className="felx w-full justify-center items-center">
       <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
@@ -46,18 +59,47 @@ const Welcome = () => {
         <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10">
           <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
             <div className="flex justify-between flex-col w-full h-full">
-              <div className="flex flex-col flex-1 justify-between items-start">
+              <div className="flex justify-between items-start">
                 <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
                   <SiEthereum fontSize={21} color="#fff" />
                 </div>
-                <div>
-                  <p className="text-white font-light text-sm">Address</p>
-                  <p className="text-white font-semibold text-lg mt-1">
-                    Ethereum
-                  </p>
-                </div>
+                <BsInfoCircle fontSize={17} color="#fff" />
+              </div>
+              <div>
+                <p className="text-white font-light text-sm">Address</p>
+                <p className="text-white font-semibold text-lg mt-1">
+                  Ethereum
+                </p>
               </div>
             </div>
+          </div>
+
+          {/* form starts form here */}
+          <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
+            <Input
+              placeholder={"Address To"}
+              name="addressTo"
+              type="text"
+              handleChange={() => {}}
+            />
+            <Input
+              placeholder={"Amount (ETH)"}
+              name="amount"
+              type="number"
+              handleChange={() => {}}
+            />
+            <Input
+              placeholder={"keyword (Gif)"}
+              name="keyword"
+              type="text"
+              handleChange={() => {}}
+            />
+            <Input
+              placeholder={"Address to"}
+              name="addressTo"
+              type="text"
+              handleChange={() => {}}
+            />
           </div>
         </div>
       </div>
